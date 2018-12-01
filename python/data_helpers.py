f5e90b9987e50984eae4714b6a3b35ff77fadecb
gsiogkas <gsiogkas@cvrlab.com>
2018-12-01 18:39:13 +0000

    adding occlusions handling in ground truth reader

--- python/data_helpers.py
+++ python/data_helpers.py
@@ -29,7 +29,7 @@ def my_hook(t):
     return update_to
 
 
-def read_road_annotations(filename, shape):
+def read_road_annotations(filename, shape, object_name):
     """Function to read road annotations in DIPLODC format 
     
     Arguments:
@@ -47,7 +47,7 @@ def read_road_annotations(filename, shape):
     for line in lines:
         line = line.replace('\n', '')
         contents = line.split(' ')
-        if contents[0] == 'road':
+        if contents[0] == object_name:
             for i, sample in enumerate(contents[2:]):
                 if i % 2:
                     row.append(int (float(sample) * shape[0]) )
@@ -72,8 +72,15 @@ def read_diplodoc(base_path, name='diplo', frame_idx=0):
     im_fnm = base_path +  name + str(frame_idx).zfill(6) + '-L.png'
     im_rgb = plt.imread(im_fnm)
     road_x, road_y = read_road_annotations(im_fnm.replace('png', 'txt'),
-                                           np.shape(im_rgb)[:2])
+                                           np.shape(im_rgb)[:2], 
+                                           'road')
+    occl_x, occl_y = read_road_annotations(im_fnm.replace('png', 'txt'),
+                                           np.shape(im_rgb)[:2], 
+                                           'occl')
     road_mask = poly2mask(road_x, road_y, np.shape(im_rgb)[:2])
+    if occl_x:
+        occl_mask = poly2mask(occl_x, occl_y, np.shape(im_rgb)[:2])
+        road_mask = np.bitwise_and(road_mask, np.bitwise_not(occl_mask))
     
     road_mask[-1, :] = True
     for i in np.where(road_mask[:,-2]==True)[0]:
@@ -109,3 +116,9 @@ def download_decompress_diplodoc():
 
 if __name__ == '__main__':
     pass
+
+
+        
+
+
+
