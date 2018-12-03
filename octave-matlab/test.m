3d6d7b25afa9a486d3c4c4480c9d1793cceab9d8
gsiogkas <gsiogkas@cvrlab.com>
2018-12-03 11:39:21 +0100

    metrics on original size

--- octave-matlab/test.m
+++ octave-matlab/test.m
@@ -5,9 +5,8 @@ function [TP, FP, FN, R, P, Q, F1, IM] = test
     im2 = imread('../test/diplo000001-L.png');
     [xyroad, xyoccl, gt2] = read_labeled_data('../test/diplo000001-L.txt');
     [det2, probabilities, labels] = road_detection(im1, im2);
-    [TP, FP, FN, R, P, Q, F1, IM] = calculate_statistics(det2, ...
-                                                         imresize(gt2, ...
-                                                         size(det2), 'nearest'));
+    det2 = imresize(det2, size(gt2), 'nearest');
+    [TP, FP, FN, R, P, Q, F1, IM] = calculate_statistics(det2, gt2);
     fprintf('True Positives = %f \n', TP)
     fprintf('False Positives = %f \n', FP)
     fprintf('False Negatives = %f \n', FN)
