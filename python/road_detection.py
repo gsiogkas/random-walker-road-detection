ee696f71d158c5e6514340fed80c57f3c55f0903
gsiogkas <gsiogkas@cvrlab.com>
2023-08-07 18:00:16 +0300

    Update road_detection.py

--- python/road_detection.py
+++ python/road_detection.py
@@ -1,6 +1,6 @@
 # MIT License
 
-# Copcol_roadight (c) 2018 George Siogkas
+# Copyright (c) 2018 George Siogkas
 
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
